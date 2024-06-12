1b2a44cceb98cd6411331f2de72134a87ccdec64
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2024-06-12 11:24:20 +0700

    Update GroupingID.sql

--- Groupby/GroupingID.sql
+++ Groupby/GroupingID.sql
@@ -5,3 +5,15 @@ SELECT
 ,	SUM(Amount) as Amount
 FROM Sales.vwTestAmount
 GROUP BY ROLLUP (Country,OrderYear,Category)
+GO
+
+SELECT 
+	Country,OrderYear,Category
+--,	GROUPING_ID(Country) as GID1 ,GROUPING_ID(OrderYear) as GID2 ,GROUPING_ID(Category) as GID3
+--,	GROUPING_ID(Country,OrderYear,Category) as GID4
+,	SUM(Amount) as Amount
+FROM Sales.vwTestAmount
+GROUP BY 
+	GROUPING SETS 
+	((),(Country),(Country,OrderYear),(Country,OrderYear,Category))
+GO
