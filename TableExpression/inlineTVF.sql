e68a9203c768c7a730b0f39916395d7ec62851c8
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2024-06-12 13:40:50 +0700

    Update inlineTVF.sql

--- TableExpression/inlineTVF.sql
+++ TableExpression/inlineTVF.sql
@@ -1,1 +1,9 @@
 --Inline TVF
+CREATE OR ALTER FUNCTION Production.udfGetProductByCatiD(@catid int,@rowno int)RETURNS TABLE
+AS
+	RETURN
+		SELECT TOP (@rowno)
+			* 
+		FROM Production.Products
+		WHERE categoryid=@catid
+		ORDER BY unitprice DESC
