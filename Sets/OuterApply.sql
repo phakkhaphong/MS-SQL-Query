b66824617cbc545fe8c204d27aaaf43a372b3da9
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2024-06-12 14:42:42 +0700

    Update OuterApply.sql

--- Sets/OuterApply.sql
+++ Sets/OuterApply.sql
@@ -4,5 +4,7 @@ SELECT
 FROM Sales.Customers as C
 OUTER APPLY
 	(
-	SELECT TOP 2 * FROM Sales.Orders as O WHERE O.custid=C.custid
+	SELECT TOP 2 --Bucket Size
+	* 
+	FROM Sales.Orders as O WHERE O.custid=C.custid
 	)as X
